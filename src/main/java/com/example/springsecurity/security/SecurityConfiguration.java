56dc268491b456aa0b24f006e4cbe9c27e3e8529
Ruma <matteoh8@hotmail.it>
2025-05-10 13:59:44 +0200

    ok

--- src/main/java/com/example/springsecurity/security/SecurityConfiguration.java
+++ src/main/java/com/example/springsecurity/security/SecurityConfiguration.java
@@ -1,11 +1,15 @@
 package com.example.springsecurity.security;
 
-import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -16,24 +20,31 @@ public class SecurityConfiguration {
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
-        // http.formLogin(withDefaults()); // Disattiviamo il login basato su form
-
-        http.sessionManagement(session ->
-                session.sessionCreationPolicy(SessionCreationPolicy.STATELESS) // Disabilita la gestione della sessione
-        );
+        http.authorizeHttpRequests((requests) -> requests
+                .anyRequest().authenticated());
+        http.sessionManagement(session -> session
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         http.httpBasic(withDefaults());
-
-        // Il logout viene disabilitato di default.
-        // Possiamo riabilitarlo esplicitamente
-//        http.logout(logout -> logout
-//                .logoutUrl("/logout") // URL per il logout
-//                .logoutSuccessHandler((request, response, authentication) -> {
-//                    response.setStatus(HttpServletResponse.SC_OK); // Restituisce 200 OK al successo
-//                })
-//                .invalidateHttpSession(true) // Invalida la sessione HTTP
-//                .clearAuthentication(true) // Cancella l'autenticazione
-//        );
         return http.build();
     }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        // Creiamo un utente in memoria con nome utente "user1" e password "password1"
+        // {noop} indica che non viene utilizzata alcuna codifica della password
+        UserDetails user = User.withUsername("user")
+                .password("{noop}pass")
+                .roles("USER")
+                .build();
+
+        // Creiamo un secondo utente in memoria con nome utente "admin" e password "admin"
+        UserDetails admin = User.withUsername("admin")
+                .password("{noop}admin")
+                .roles("ADMIN")
+                .build();
+
+        // Il costruttore accetta una lista di utenti
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+
 }
